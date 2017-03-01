99760fee56654c552699a086caa8e6a2db4b8a31
Victor <victor.gomes@convenia.com.br>
2017-03-01 09:14:11 -0300

    creating product component

--- ionic2/ionicPagSeg/src/app/app.module.ts
+++ ionic2/ionicPagSeg/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { ProductListPage } from '../pages/product-list/product-list';
 
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ProductListPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,7 +22,8 @@ import { ListPage } from '../pages/list/list';
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ProductListPage,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
